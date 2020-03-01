8d5fd3433b705d6e1f00c33de7dcad5a9e4107a1
Josh <joshkarlin@hotmail.co.uk>
2020-03-01 19:23:37 +0000

    :sparkles: set up app/__init__.py with config, redis & rq
    In app/__init__.py, the create_app function now configures the app based
    on app/config.py, and sets up the redis connection and the rq jobs queue

--- {{cookiecutter.project_name}}/app/__init__.py
+++ {{cookiecutter.project_name}}/app/__init__.py
@@ -1,2 +1,17 @@
-def create_app():
-    return
+from flask import Flask
+from redis import Redis
+from rq import Queue
+
+from app.config import get_config_class
+
+
+def create_app() -> Flask:
+    """Creates a flask app, sets up config and registers required add-ons."""
+    app = Flask(__name__)
+    app.config.from_object(get_config_class())
+
+    # Setup redis and rq
+    app.redis = Redis.from_url(app.config["REDIS_URL"])
+    app.task_queue = Queue("default", connection=app.redis)
+
+    return app
