f4c52c8ef8ece9da61f6ba6a7ec9a13cfd544cc4
Josh <joshkarlin@hotmail.co.uk>
2020-03-01 19:27:41 +0000

    :sparkles: create rq_tasks dir and make a dummy_task
    There is now a directory for rq tasks: app/rq_tasks. Tasks are defined
    in separate files and then imported into the __init__.py file in the
    rq_tasks directory. A dummy_task has been created which sleeps for the
    given number of seconds.

---
+++ {{cookiecutter.project_name}}/app/rq_tasks/__init__.py
@@ -0,0 +1,1 @@
+from app.rq_tasks.dummy_task import dummy_task  # noqa
---
+++ {{cookiecutter.project_name}}/app/rq_tasks/dummy_task.py
@@ -0,0 +1,6 @@
+import time
+
+
+def dummy_task(nap_length: int = 420):
+    """Nap for the given amount of time."""
+    time.sleep(nap_length)
